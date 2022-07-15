ab55efcf4ee8066a0eaaf106159b8fbd11a99b04
nouvelle <nouvellenuts@gmail.com>
2022-07-15 19:16:43 +0900

    rubocop のエラー修正

--- 04.bowling/index.rb
+++ 04.bowling/index.rb
@@ -1,53 +1,51 @@
+# frozen_string_literal: true
+
 # 引数取得
 args = ARGV[0]
 
 # １投毎に分ける
-scoreArr = args.split(",")
+score_arr = args.split(',')
 
 # フレームごとに分割
 scores = []
-tmpScore = []
-scoreArr.each_with_index do |score, index|
+tmp_score = []
+score_arr.each_with_index do |score, index|
   # 最終フレームの場合
   if scores.size == 9
-    score = "10" if score == "X"
-    tmpScore.push(score)
-    if index == scoreArr.size - 1
-      scores.push(tmpScore)
-      tmpScore = []
+    score = '10' if score == 'X'
+    tmp_score.push(score)
+    if index == score_arr.size - 1
+      scores.push(tmp_score)
+      tmp_score = []
     end
-  else
-    if score == "X"
-      scores.push(["10"])
-    elsif tmpScore.size == 0
-      tmpScore.push(score)
-    elsif tmpScore.size == 1
-      tmpScore.push(score)
-      scores.push(tmpScore)
-      tmpScore = []
-    end
+  elsif score == 'X'
+    scores.push(['10'])
+  elsif tmp_score.empty?
+    tmp_score.push(score)
+  elsif tmp_score.size == 1
+    tmp_score.push(score)
+    scores.push(tmp_score)
+    tmp_score = []
   end
 end
 
 # 計算処理
-scoresSum = 0
+scores_sum = 0
 scores.each_with_index do |frame, index|
-  frameSum = frame.sum { |n| n.to_i }
+  frame_sum = frame.sum(&:to_i)
   if index == 9
-    scoresSum += frameSum
+    scores_sum += frame_sum
+  elsif frame[0] == '10'
+    next_score = scores[index + 1][0].to_i
+    next_next_score = scores[index + 1][1] ? scores[index + 1][1].to_i : scores[index + 2][0].to_i
+    scores_sum += (10 + next_score + next_next_score)
+  elsif frame_sum == 10
+    next_score = scores[index + 1][0].to_i
+    scores_sum += (frame_sum + next_score)
   else
-    if frame[0] == "10"
-      nextScore = scores[index + 1][0].to_i
-      nextNextScore = scores[index + 1][1] ? scores[index + 1][1].to_i : scores[index + 2][0].to_i
-      scoresSum += (10 + nextScore + nextNextScore)
-    elsif frameSum == 10
-      nextScore = scores[index + 1][0].to_i
-      scoresSum += (frameSum + nextScore)
-    else
-      scoresSum += frameSum
-    end
+    scores_sum += frame_sum
   end
 end
 
 # 最終出力
-puts scoresSum
+puts scores_sum
