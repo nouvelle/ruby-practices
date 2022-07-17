4e71d4ab65b0be3188aff28934946754b9b03821
nouvelle <nouvellenuts@gmail.com>
2022-07-18 01:07:18 +0900

    rubocop エラー対応

--- 05.ls/ls.rb
+++ 05.ls/ls.rb
@@ -1,33 +1,33 @@
 #!/usr/bin/env ruby
 
+# frozen_string_literal: true
+
 COLUMN_NUMBER = 3
 filenames = Dir.glob('*')
-maxLength = 0
+max_length = 0
 
 # ファイルデータ格納配列の初期値作成
-def set_initial_arr(num)
+def make_initial_arr(num)
   result = []
-  (1..num).each do |num|
-    result << []
-  end
+  (1..num).each { result << [] }
   result
 end
 
 # ファイル名表示メソッド
-def print_filenames(filenames_arr, maxLength)
-  return if filenames_arr.size == 0
-  puts filenames_arr.map { |name| name.ljust(maxLength + 1)}.join
+def print_filenames(filenames_arr, max_length)
+  return if filenames_arr.empty?
+
+  puts filenames_arr.map { |name| name.ljust(max_length + 1) }.join
 end
 
-result = set_initial_arr(COLUMN_NUMBER)
+result = make_initial_arr(COLUMN_NUMBER)
 
 filenames.each_with_index do |name, index|
   remainder = index % COLUMN_NUMBER
-  maxLength = name.size if maxLength < name.size
+  max_length = name.size if max_length < name.size
   result[remainder] << name
-
 end
 
-print_filenames(result[0], maxLength)
-print_filenames(result[1], maxLength)
-print_filenames(result[2], maxLength)
+print_filenames(result[0], max_length)
+print_filenames(result[1], max_length)
+print_filenames(result[2], max_length)
