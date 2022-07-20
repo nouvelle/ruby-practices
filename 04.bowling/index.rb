47add371bb496782eda0d3be1121a7b5dfa9049b
nouvelle <nouvellenuts@gmail.com>
2022-07-21 01:32:35 +0900

    指摘修正

--- 04.bowling/index.rb
+++ 04.bowling/index.rb
@@ -6,46 +6,50 @@ args = ARGV[0]
 # １投毎に分ける
 score_arr = args.split(',')
 
-# フレームごとに分割
-scores = []
-tmp_score = []
+# 文字列を数値に変換、'X' を 10 に変換
 score_arr.each_with_index do |score, index|
-  # 最終フレームの場合
-  if scores.size == 9
-    score = '10' if score == 'X'
-    tmp_score.push(score)
-    if index == score_arr.size - 1
-      scores.push(tmp_score)
-      tmp_score = []
-    end
-  elsif score == 'X'
-    scores.push(['10'])
-  elsif tmp_score.empty?
-    tmp_score.push(score)
-  elsif tmp_score.size == 1
-    tmp_score.push(score)
-    scores.push(tmp_score)
-    tmp_score = []
+  score_arr[index] = if score == 'X'
+                       10
+                     else
+                       score.to_i
+                     end
+end
+
+# 2投球を1フレームとしてまとめる
+scores = []
+prev_frame = []
+score_arr.each do |score|
+  prev_frame.push(score)
+  if prev_frame.size == 1 && score == 10
+    scores.push(prev_frame)
+    prev_frame = []
+  elsif prev_frame.size == 2
+    scores.push(prev_frame)
+    prev_frame = []
   end
 end
 
+# 最終フレームの場合の処理
+frame10 = scores[9..11].flatten
+scores = scores[0..8]
+scores.concat([frame10])
+
 # 計算処理
-scores_sum = 0
-scores.each_with_index do |frame, index|
-  frame_sum = frame.sum(&:to_i)
+total = scores.each_with_index.sum do |frame, index|
+  frame_sum = frame.sum
   if index == 9
-    scores_sum += frame_sum
-  elsif frame[0] == '10'
-    next_score = scores[index + 1][0].to_i
-    next_next_score = scores[index + 1][1] ? scores[index + 1][1].to_i : scores[index + 2][0].to_i
-    scores_sum += (10 + next_score + next_next_score)
+    frame_sum
+  elsif frame[0] == 10
+    next_score = scores[index + 1][0]
+    next_next_score = scores[index + 1][1] || scores[index + 2][0]
+    10 + next_score + next_next_score
   elsif frame_sum == 10
-    next_score = scores[index + 1][0].to_i
-    scores_sum += (frame_sum + next_score)
+    next_score = scores[index + 1][0]
+    frame_sum + next_score
   else
-    scores_sum += frame_sum
+    frame_sum
   end
 end
 
 # 最終出力
-puts scores_sum
+puts total
