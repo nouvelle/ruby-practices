fffdc2e495cf614858668196c6805832bed2f99a
nouvelle <nouvellenuts@gmail.com>
2022-08-15 23:36:36 +0900

    feat: 標準入力にも対応

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -1,19 +1,7 @@
 #!/usr/bin/env ruby
 # frozen_string_literal: true
 
-filename = ARGV[0]
-
-if ARGV[0]
-  arg = ARGV[0]
-  if arg[0] == '-'
-    is_l = arg.include?('l')
-    is_w = arg.include?('w')
-    is_c = arg.include?('c')
-    filename = ARGV[1]
-  end
-end
-
-strings = File.read(filename)
+args = ARGV
 
 # 行数
 def line_count(str)
@@ -32,10 +20,43 @@ def file_size(file)
   fs.size
 end
 
-result = ''
-result += format('% 8d', line_count(strings)) if is_l
-result += format('% 8d', word_count(strings)) if is_w
-result += format('% 8d', file_size(filename)) if is_c
-result += " #{filename}"
+# 標準入力
+def handle_stdin(str)
+  result = ''
+  result += format('% 8d', line_count(str))
+  result += format('% 8d', word_count(str))
+  result += format('% 8d', str.size)
+  puts result
+end
 
-puts result
+# パラメータ指定
+def handle_param(filename, is_l, is_w, is_c)
+  strings = File.read(filename)
+  result = ''
+  result += format('% 8d', line_count(strings)) if is_l
+  result += format('% 8d', word_count(strings)) if is_w
+  result += format('% 8d', file_size(filename)) if is_c
+  result += " #{filename}"
+  puts result
+end
+
+if args.size == 0
+  stdin = ARGF.gets('')
+  handle_stdin(stdin)
+else
+  filename = args[0]
+  
+  if args[0]
+    arg = args[0]
+    if arg[0] == '-'
+      is_l = arg.include?('l')
+      is_w = arg.include?('w')
+      is_c = arg.include?('c')
+    end
+    if args[1]
+      filename = args[1]
+    end
+  end
+
+  handle_param(filename, is_l, is_w, is_c)
+end
