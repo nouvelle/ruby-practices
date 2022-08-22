04afcb768ddaa465da5f156f20d123225e87fa39
nouvelle <nouvellenuts@gmail.com>
2022-08-23 00:02:57 +0900

    fix: 変数名修正

--- 06.wc/wc.rb
+++ 06.wc/wc.rb
@@ -23,8 +23,8 @@ end
 def calc_string(string, is_l, is_w, is_c, is_all)
   lines_part = format('% 8d', line_count(string)) if is_l || is_all
   words_part = format('% 8d', word_count(string)) if is_w || is_all
-  counts_part = format('% 8d', file_size(string)) if is_c || is_all
-  [lines_part, words_part, counts_part]
+  bytes_part = format('% 8d', file_size(string)) if is_c || is_all
+  [lines_part, words_part, bytes_part]
 end
 
 if args[0]
@@ -46,16 +46,16 @@ total_c = 0
 
 if File.pipe?($stdin)
   stdin = $stdin.gets('')
-  lines_part, words_part, counts_part = calc_string(stdin, is_l, is_w, is_c, is_all)
-  puts "#{lines_part}#{words_part}#{counts_part}"
+  lines_part, words_part, bytes_part = calc_string(stdin, is_l, is_w, is_c, is_all)
+  puts "#{lines_part}#{words_part}#{bytes_part}"
 else
   filenames.each do |filename|
     string_of_file = File.read(filename)
-    lines_part, words_part, counts_part = calc_string(string_of_file, is_l, is_w, is_c, is_all)
+    lines_part, words_part, bytes_part = calc_string(string_of_file, is_l, is_w, is_c, is_all)
     total_l += lines_part.to_i
     total_w += words_part.to_i
-    total_c += counts_part.to_i
-    puts "#{lines_part}#{words_part}#{counts_part} #{filename}"
+    total_c += bytes_part.to_i
+    puts "#{lines_part}#{words_part}#{bytes_part} #{filename}"
   end
   return unless filenames.size != 1
 
